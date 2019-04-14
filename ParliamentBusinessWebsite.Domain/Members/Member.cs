c1748812026c35a45e3ce5b845aad968ce3a8c40
Gary Moore <garyprm@gmail.com>
2019-04-14 14:04:59 +0100

    Implement business home page, add angular js (#1)

---
+++ ParliamentBusinessWebsite.Domain/Members/Member.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace ParliamentBusinessWebsite.Domain.Members
+{
+    public class Member
+    {
+        [XmlAttribute("Member_Id")]
+        public int Id { get; set; }
+
+        public string DisplayAs { get; set; }
+
+        public string ListAs { get; set; }
+
+        public string FullTitle { get; set; }
+    }
+}
---
+++ ParliamentBusinessWebsite.Services/Members/MemberService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ParliamentBusinessWebsite.Domain.Members;
+
+namespace ParliamentBusinessWebsite.Services.Members
+{
+    public class MemberService : IMemberService
+    {
+        private string MemberUrl = "http://data.parliament.uk/membersdataplatform/services/mnis/members/query/";
+        private readonly IHttpClientFactory _clientFactory;
+
+        public MemberService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<Member> Get(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{MemberUrl}id={id}");
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var serialiser = new XmlSerializer(typeof(Member), new XmlRootAttribute("Members"));
+                var content = await response.Content.ReadAsStreamAsync();
+                return ((IList<Member>)serialiser.Deserialize(content)).FirstOrDefault();
+            }
+            else
+            {
+                throw new Exception("Error");
+            }
+        }
+    }
+}
